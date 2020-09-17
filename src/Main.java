a91b0b71d5a18cb94f0daaa79b4c796f676c0e7e
AsanS <70992040+AsanSh@users.noreply.github.com>
2020-09-18 01:18:44 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,10 @@ public class Main {
     public static int bossHealth = 700;
     public static int bossDamage = 50;
     public static String bossDefence = "";
-    public static int[] heroesHealth = {260, 250, 240, 300};
-    public static int[] heroesDamage = {20, 25, 15, 0};
-    public static String[] heroesAttackType = {"Physical",
-            "Magical", "Kinetic", "Medic"};
+    public static int[] heroesHealth = {260, 250, 240, 300, 500, 1, 1};
+    public static int[] heroesDamage = {20, 25, 15, 1, 1, 1};
+    public static String[] heroesAttackType = {"Physical", "Magical", "Kinetic", "Medic", "Golem", "Berserk", "Thor"};
+
 
     public static void main(String[] args) {
         printStatistics();
@@ -16,12 +16,6 @@ public class Main {
             round();
         }
     }
-    /*
-    Добавить 4-го игрока Medic, у которого есть способность лечить после каждого
-    раунда на N-ное количество единиц здоровья только одного из членов команды,
-    имеющего здоровье менее 100 единиц. Мертвых героев медик оживлять не может,
-    и лечит он до тех пор пока жив сам. Медик не участвует в бою, но получает урон от Босса
-     */
 
     public static void changeBossDefence() {
         Random r = new Random();
@@ -32,6 +26,8 @@ public class Main {
 
     public static void round() {
         changeBossDefence();
+        medicSelection();
+        thor();
         heroesHit();
         if (bossHealth > 0) {
             bossHits();
@@ -47,6 +43,9 @@ public class Main {
                     int coeff = r.nextInt(6) + 2; //2,3,4,5,6,7,8,9
                     System.out.println("Critical Damage: " +
                             heroesDamage[i] * coeff);
+                    if (heroesAttackType[4] == bossDefence) {
+                        heroesHealth[i] = heroesHealth[i] + bossDamage / 5;
+                    }
                     if (bossHealth - heroesDamage[i] * coeff < 0) {
                         bossHealth = 0;
                     } else {
@@ -66,8 +65,15 @@ public class Main {
     public static void bossHits() {
         Random r = new Random();
         int chance = r.nextInt(3); // 0, 1, 2
-        double coeff = Math.random(); // какое-то число от 0 до 1 (0.378)
-        if(chance == 0) {
+        int coeff = r.nextInt(100); // какое-то число от 0 до 1 (0.378)
+        int lucky = r.nextInt(3);
+        if (lucky == 0) {
+            System.out.println("он укланился от удара " + bossDamage * 0);
+        }
+        if (heroesAttackType[5] == bossDefence) {
+            heroesHealth[5] = heroesHealth[5] + bossDamage * 2;
+        }
+        if (chance == 0) {
             System.out.println("Boss became kind " + (int) (bossDamage * coeff));
         }
         for (int i = 0; i < heroesHealth.length; i++) {
@@ -89,6 +95,25 @@ public class Main {
         }
     }
 
+    public static void medicSelection() {
+
+        for (int i = 0; i < heroesAttackType.length; i += 20) {
+            if (heroesAttackType[3] == bossDefence) {
+                if (heroesHealth[i] < 100) {
+                    heroesHealth[i] = heroesHealth[i] + 99999;
+                }
+            }
+
+        }
+    }
+
+    public static void thor() {
+        if (heroesAttackType[6] == bossDefence) {
+            bossDamage = 0;
+            System.out.println("босс пропускает раунд");
+        }
+    }
+
     public static void printStatistics() {
         System.out.println("________________");
         System.out.println("Boss health: " + bossHealth);
@@ -106,7 +131,7 @@ public class Main {
         }
         boolean allHeroesDead = true;
         for (int i = 0; i < heroesHealth.length; i++) {
-            if(heroesHealth[i] > 0){
+            if (heroesHealth[i] > 0) {
                 allHeroesDead = false;
                 break;
             }
@@ -116,10 +141,21 @@ public class Main {
 }
 /*
 ДЗ на сообразительность:
-● Добавить n-го игрока, Golem, который имеет увеличенную жизнь но слабый удар. Может принимать на себя 1/5 часть урона исходящего от босса по другим игрокам.
-● Добавить n-го игрока, Lucky, имеет шанс уклонения от ударов босса.
+● Добавить n-го игрока, Golem, который имеет увеличенную жизнь но слабый удар.
+Может принимать на себя 1/5 часть урона исходящего от босса по другим игрокам.!!!!!!!!!!!!!!!
+
+● Добавить n-го игрока, Lucky, имеет шанс уклонения от ударов босса.!!!!!!!!!!!!!!
+
+
 ● Добавить n-го игрока, Berserk, блокирует часть удара босса по себе и прибавляет
-заблокированный урон к своему урону и возвращает его боссу
+заблокированный урон к своему урону и возвращает его боссу!!!!!!!!!!!!!!!!!!!!!
+
 ● Добавить n-го игрока, Thor, удар по боссу имеет шанс оглушить босса на 1 раунд,
-вследствие чего босс пропускает 1 раунд и не наносит урон героям.
-*/
+вследствие чего босс пропускает 1 раунд и не наносит урон героям.!!!!!!!!!!!!!!!!!!!!
+
+    Добавить 4-го игрока Medic, у которого есть способность лечить после каждого
+    раунда на N-ное количество единиц здоровья только одного из членов команды,
+    имеющего здоровье менее 100 единиц. Мертвых героев медик оживлять не может,
+    и лечит он до тех пор пока жив сам. Медик не участвует в бою, но получает урон от Босса!!!!!!!!!
+     */
+
